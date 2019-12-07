c280699d607e70ae916338d1c61a17f458befb24
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2019-12-07 13:21:33 +0100

    Add gallery.js

---
+++ static/js/gallery.js
@@ -0,0 +1,36 @@
+$(function () {
+    $("#tree").fancytree({
+        activeVisible: true,
+        autoActivate: true,
+        autoCollapse: true,
+        autoScroll: false,
+        clickFolderMode: 2,
+        extensions: ["filter"],
+        filter: {
+            counter: false,
+            mode: "hide"
+        },
+        selectMode: 1,
+        click: function(event, data) {
+            if (data.node.isFolder() == false && typeof data.node.data.src !== 'undefined') {
+                var $videotitle = $("#videotitle");
+                var $videoplayer = $("#videoplayer");
+                $videotitle.text(data.node.data.name);
+                $videoplayer.attr("src", data.node.data.src);
+            }
+        }
+    });
+    
+    var $treeFilter = $("#treeFilter");
+    var $tree = $.ui.fancytree.getTree("#tree");
+    $treeFilter.keyup(function() {
+        var txt = $treeFilter.val();
+        if (txt !== "") {
+            $tree.filterNodes(txt);
+        }
+        else {
+            $tree.clearFilter();
+        }
+    });
+
+});
