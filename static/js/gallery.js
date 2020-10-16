f9ae3ed517943aea09eff7b2c78fc498c1e02ad4
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-10-16 15:48:01 +0200

    Disable right click context menu on the gallery page

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,4 +1,9 @@
 $(function () {
+    
+    $(this).bind("contextmenu", function(e) {
+            e.preventDefault();
+    });
+
     $("#tree").fancytree({
         activeVisible: false,
         autoActivate: false,
